49e3adf3e50e494c8349da8acb83d12c5f6ca81a
nitin54455 <nitin45344@gmail.com>
2025-06-17 11:33:36 +0530

    modified:   p1/script.js

--- p1/script.js
+++ p1/script.js
@@ -17,14 +17,22 @@ const validateUser = () => {
 const loginForm = () => {
   const str = `<div>
     <h3>Login Form</h3>
-    <p id='errorTxt'></p>
-    <p><input type='text' id='txtEmail1'></p>
-    <p><input type='password' id='txtPass1'></p>
+    <p id='errorTxt' style='color: red;'></p>
+    <p>
+      <label for='txtEmail1'>Email:</label><br>
+      <input type='text' id='txtEmail1'>
+    </p>
+    <p>
+      <label for='txtPass1'>Password:</label><br>
+      <input type='password' id='txtPass1'>
+    </p>
     <p><button onclick='validateUser()'>Submit</button></p>
     <p><button onclick='registerForm()'>Create Account</button></p>
-    `;
-  root.innerHTML = str + "</div>";
+  </div>`;
+  
+  root.innerHTML = str;
 };
+
 
 const saveUser = () => {
   let name = document.getElementById("txtName").value;
@@ -41,14 +49,25 @@ const saveUser = () => {
 const registerForm = () => {
   const str = `<div>
     <h3>Registration Form</h3>
-    <p><input type='text' id='txtName'></p>
-    <p><input type='text' id='txtEmail'></p>
-    <p><input type='password' id='txtPass'></p>
+    <p>
+      <label for='txtName'>Name:</label><br>
+      <input type='text' id='txtName'>
+    </p>
+    <p>
+      <label for='txtEmail'>Email:</label><br>
+      <input type='text' id='txtEmail'>
+    </p>
+    <p>
+      <label for='txtPass'>Password:</label><br>
+      <input type='password' id='txtPass'>
+    </p>
     <p><button onclick='saveUser()'>Submit</button></p>
     <p><button onclick='loginForm()'>Already a member? Login here...</button></p>
-    `;
-  root.innerHTML = str + "</div>";
+  </div>`;
+  
+  root.innerHTML = str;
 };
+
 
 const showHome = () => {
   const str = `<div>
