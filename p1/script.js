83e66bd5ecf17e8c7d51b9ed8bbab9bcac6792de
nitin54455 <nitin45344@gmail.com>
2025-06-17 11:28:19 +0530

    modified:   p1/script.js

--- p1/script.js
+++ p1/script.js
@@ -1,51 +1,59 @@
-const loginForm = () => {
-    const str = `
-        <div>
-            <h3>Login Form</h3>
-            <p>
-                <label>Email:</label><br/>
-                <input type="email" id="email" placeholder="Enter your email" />
-            </p>
-            <p>
-                <label>Password:</label><br/>
-                <input type="password" id="password" placeholder="Enter your password" />
-            </p>
-            <p><button onclick='showHome()'>Submit</button></p>
-            <p><button onclick='registerForm()'>Create Account</button></p>
-        </div>
-    `;
-    root.innerHTML = str;
+let users = [];
+let user = {};
+
+const validateUser = () => {
+  let email = document.getElementById("txtEmail1").value;
+  let pass = document.getElementById("txtPass1").value;
+  const found = users.find(
+    (value) => value.email === email && value.pass === pass
+  );
+  if (found) {
+    showHome();
+  } else {
+    document.getElementById("errorTxt").innerHTML = "Access Denied";
+  }
 };
 
+const loginForm = () => {
+  const str = `<div>
+    <h3>Login Form</h3>
+    <p id='errorTxt'></p>
+    <p><input type='text' id='txtEmail1'></p>
+    <p><input type='password' id='txtPass1'></p>
+    <p><button onclick='validateUser()'>Submit</button></p>
+    <p><button onclick='registerForm()'>Create Account</button></p>
+    `;
+  root.innerHTML = str + "</div>";
+};
+
+const saveUser = () => {
+  let name = document.getElementById("txtName").value;
+  let email = document.getElementById("txtEmail").value;
+  let pass = document.getElementById("txtPass").value;
+  users.push({
+    name,
+    email,
+    pass,
+  });
+  loginForm();
+};
 
 const registerForm = () => {
-    const str = `
-        <div>
-            <h3>Registration Form</h3>
-            <p>
-                <label>Name:</label><br/>
-                <input type="text" id="regName" placeholder="Enter your name" />
-            </p>
-            <p>
-                <label>Email:</label><br/>
-                <input type="email" id="regEmail" placeholder="Enter your email" />
-            </p>
-            <p>
-                <label>Password:</label><br/>
-                <input type="password" id="regPassword" placeholder="Enter your password" />
-            </p>
-            <p><button onclick='loginForm()'>Submit</button></p>
-            <p><button onclick='loginForm()'>Already a member? Login here...</button></p>
-        </div>
+  const str = `<div>
+    <h3>Registration Form</h3>
+    <p><input type='text' id='txtName'></p>
+    <p><input type='text' id='txtEmail'></p>
+    <p><input type='password' id='txtPass'></p>
+    <p><button onclick='saveUser()'>Submit</button></p>
+    <p><button onclick='loginForm()'>Already a member? Login here...</button></p>
     `;
-    root.innerHTML = str;
+  root.innerHTML = str + "</div>";
 };
 
-
 const showHome = () => {
-     const str = `<div>
+  const str = `<div>
     <h3>Welcome</h3>
     <p><button onclick='loginForm()'>Logout</button></p>
-    `
-    root.innerHTML = str + "</div>"
-}
+    `;
+  root.innerHTML = str + "</div>";
+};
