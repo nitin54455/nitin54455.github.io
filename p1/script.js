ea3842a661030884d208cb2f158789a0a4a32cfb
nitin54455 <nitin45344@gmail.com>
2025-06-17 10:44:05 +0530

    modified:   p1/index.html 	modified:   p1/script.js 	renamed:    p1/style.css -> p1/styles.css

--- p1/script.js
+++ p1/script.js
@@ -1,20 +1,46 @@
 const loginForm = () => {
-    const str = `<div>
-    <h3>Login Form</h3>
-    <p><button onclick='showHome()'>Submit</button></p>
-    <p><button onclick='registerForm()'>Create Account</button></p>
-    `
-    root.innerHTML = str + "</div>"
-}
+    const str = `
+        <div>
+            <h3>Login Form</h3>
+            <p>
+                <label>Email:</label><br/>
+                <input type="email" id="email" placeholder="Enter your email" />
+            </p>
+            <p>
+                <label>Password:</label><br/>
+                <input type="password" id="password" placeholder="Enter your password" />
+            </p>
+            <p><button onclick='showHome()'>Submit</button></p>
+            <p><button onclick='registerForm()'>Create Account</button></p>
+        </div>
+    `;
+    root.innerHTML = str;
+};
+
 
 const registerForm = () => {
-     const str = `<div>
-    <h3>Registration Form</h3>
-    <p><button onclick='loginForm()'>Submit</button></p>
-    <p><button onclick='loginForm()'>Already a member? Login here...</button></p>
-    `
-    root.innerHTML = str + "</div>"
-}
+    const str = `
+        <div>
+            <h3>Registration Form</h3>
+            <p>
+                <label>Name:</label><br/>
+                <input type="text" id="regName" placeholder="Enter your name" />
+            </p>
+            <p>
+                <label>Email:</label><br/>
+                <input type="email" id="regEmail" placeholder="Enter your email" />
+            </p>
+            <p>
+                <label>Password:</label><br/>
+                <input type="password" id="regPassword" placeholder="Enter your password" />
+            </p>
+            <p><button onclick='loginForm()'>Submit</button></p>
+            <p><button onclick='loginForm()'>Already a member? Login here...</button></p>
+        </div>
+    `;
+    root.innerHTML = str;
+};
+
 
 const showHome = () => {
      const str = `<div>
