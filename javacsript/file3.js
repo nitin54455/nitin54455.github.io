d9e981338f08f882513cce5c78e76ad2ce25346a
nitin54455 <nitin45344@gmail.com>
2025-06-20 10:19:01 +0530

    updated

---
+++ javacsript/file3.js
@@ -0,0 +1,33 @@
+// let a=10
+// let b=12
+// if (a>b) {
+//     console.log(`${a} i greater`)
+// }
+// else{
+//     console.log(`${b} is greater`)
+// }
+
+
+// if only 1 line is there is if statement then curly bracket can be removed 
+
+// if (a>b)    console.log(`${a} i greater`)
+// else    console.log(`${b} is greater`)
+
+
+// let c
+// if(c) console.log("Good morning")
+// else console.log("Good afternoon")
+
+
+// let c=null
+// if(c) console.log("Good morning")
+// else console.log("Good afternoon")
+
+
+let v1=10
+let v2=20
+let v3=15
+if (v1>v2 && v1>v3) console.log("v1 is greater ")
+else if (v2>v1 && v2>v3) console.log("v2 is greater");
+else console.log("v3 is greater")
+    
