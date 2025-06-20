9c1a016a917251556f87e82c4b9af49822d74e6b
nitin54455 <nitin45344@gmail.com>
2025-06-20 09:58:20 +0530

    modified:   javacsript/file2.js 	new file:   javacsript/file3.js 	modified:   javacsript/index.html

--- javacsript/file2.js
+++ javacsript/file2.js
@@ -3,9 +3,18 @@
 // console.log(a+b)
 // console.log(a%b)
 
-let a="10"
-let b=10
 
-console.log(a+b)
+// let a="10"
+// let b=10
+// console.log(a+b)
+// console.log(a*b)
 
-console.log(a*b)
+
+// let a=2 // if 2 is string then also it will be true
+// let b=2
+// console.log(a==b) 
+
+
+let a=10
+let b=2
+console.log(!a>b)
